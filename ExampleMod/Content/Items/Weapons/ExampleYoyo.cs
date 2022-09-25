52b09c49dbfb3c7b3682b5bd96bbb795caf70f59
BratrilliantG7 <bratrilliantgamer7@yahoo.com>
2022-09-25 06:49:27 -0500

    Initial Yoyo Implementation

---
+++ ExampleMod/Content/Items/Weapons/ExampleYoyo.cs
@@ -0,0 +1,68 @@
+﻿using ExampleMod.Content.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace ExampleMod.Content.Items.Weapons
+{
+	// ExampleYoyo and ExampleYoyoProjectile show the minimum amound of code needed to create a Yoyo using the existing vanilla code and behavior.
+	// ExampleAdvancedYoyo and ExampleAdvancedYoyoProjectile need to be consulted if more advanced customization is required.
+
+	// ExampleYoyo is a copy of the Cascade yoyo.
+	public class ExampleYoyo : ModItem {
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Example Yoyo");
+			Tooltip.SetDefault("This is a modded Yoyo with normal behaviour.");
+			SacrificeTotal = 1; // The number of items needed to be sacrificed in the Journey Mode menu to get infinite access to them.
+
+			// The following sets are used for Gamepad controls and nothing else.
+			// Yoyo is used in the gamepad reach check in conjunction with a yoyoString check to add the String's extra range to the cursor.
+			ItemID.Sets.Yoyo[Item.type] = true;
+
+			// GamepadExtraRange is how much extra range (in Tiles) should be given to the gamepad cursor while channeling to allow free movement of the yoyo.
+			// Vanilla yoyos typically keep this number at just over the yoyo's max reach.
+			// To find what number you should use, take your desired Yoyo range (see YoyosMaximumRange in ExampleYoyoProjectile and divide that by 16, rounded up) and subtract the player's base range of 5.
+			// For example, the Cascade has a YoyosMaximumRange of 235. Divided by 16 and rounded up is 15, then subtract 5 to get 10, which is the same value the Cascade is set to have in vanilla.
+			ItemID.Sets.GamepadExtraRange[Item.type] = 10;
+
+			// GamepadSmartQuickReach is unused in vanilla code as far as I can tell, but may have been planned to force the yoyo to max range if the Smart Cursor was enabled.
+			ItemID.Sets.GamepadSmartQuickReach[Item.type] = true;
+		}
+
+		public override void SetDefaults() {
+			// These default values, aside from the Item.shoot value, match the Cascade's default values.
+
+			// The Yoyo item's hitbox width and height (more or less unused for yoyos since noMelee and noUseGraphic are set to true and their projectiles have their own width and height).
+			Item.width = 24;
+			Item.height = 24;
+
+			Item.useStyle = ItemUseStyleID.Shoot; // How you use the item (Holding Up, Swinging, etc.).
+			Item.useAnimation = 25; // The use time in game ticks (60 ticks == 1 second).
+			Item.useTime = 25; // The use time in game ticks (60 ticks == 1 second).
+
+			Item.damage = 27; // The damage your yoyo will deal (hitting enemies behind walls reduces this by about 25%).
+			Item.knockBack = 4.3f; // The yoyo's knockback.
+			Item.DamageType = DamageClass.MeleeNoSpeed; // The type of damage the Yoyo deals. MeleeNoSpeed is used so that the player's Melee Speed doesn't effect the yoyo's useTime.
+
+			Item.channel = true; // The yoyo will continually function while the mouse is held down.
+			Item.noMelee = true; // Prevents the use animation from damaging anything, that's what the yoyo projectile is for.
+			Item.noUseGraphic = true; // Hides the Yoyo graphic in the player's hands
+
+			Item.UseSound = SoundID.Item1; // The sound the item makes when used.
+			Item.value = Item.sellPrice(silver: 1, copper: 80); // This yoyo sells for 1 silver and 80 copper.
+			Item.rare = ItemRarityID.Orange; // The color of the name of your item.
+
+			Item.shootSpeed = 16f; // How fast the initial projectile will launch from the player.
+			Item.shoot = ModContent.ProjectileType<ExampleYoyoProjectile>(); // The Yoyo projectile.
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient<ExampleItem>()
+				.AddTile<Tiles.Furniture.ExampleWorkbench>()
+				.Register();
+		}
+	}
+}
---
+++ ExampleMod/Content/Projectiles/ExampleYoyoProjectile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Projectiles
+{
+	// ExampleYoyo and ExampleYoyoProjectile show the minimum amound of code needed to create a Yoyo using the existing vanilla code and behavior.
+	// ExampleAdvancedYoyo and ExampleAdvancedYoyoProjectile need to be consulted if more advanced customization is required.
+
+	// ExampleYoyoProjectile is a copy of the Cascade yoyo projectile.
+	public class ExampleYoyoProjectile : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+			// The following sets are only applicable to yoyos that use aiStyle 99.
+			// The values used here are the Cascade's default values.
+
+			// YoyosLifeTimeMultiplier is how long (in seconds) the yoyo will stay out before automatically returning to the player.
+			// Vanilla values range from 3f(Wood) to 16f(Chik), and defaults to -1. Leaving as -1 will make the time infinite.
+			ProjectileID.Sets.YoyosLifeTimeMultiplier[Projectile.type] = 13f; // The Cascade will remain active for ~13 seconds.
+
+			// YoyosMaximumRange is the maximum distance the yoyo will rest from the player. The range in tiles is approximately YoyosMaximumRange divided by 16.
+			// Vanilla values range from 130f(Wood) to 400f(Terrarian), and defaults to 200f.
+			// Values above 960f (60 tiles), or 744f if the player has yoyoString, have little to no benefit since the player normally reach beyond that range.
+			ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 235f; // The Cascade's range is approx. 14.6875 tiles.
+
+			// YoyosTopSpeed is the top speed of the yoyo projectile.
+			// Vanilla values range from 9f(Wood) to 17.5f(Terrarian), and defaults to 10f.
+			// Speeds above 18f are not recommended as the yoyo has a tendency to fly past the cursor instead of stop on it, making it unwieldy to use.
+			ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 14f;
+		}
+
+		public override void SetDefaults() {
+			Projectile.netImportant = true; // This ensures that the projectile is synced when a player joins the world.
+			Projectile.width = 16; // The width of the yoyo projectile.
+			Projectile.height = 16; // The height of the yoyo projectile.
+			Projectile.friendly = true; // Lets the projectile hit enemy NPCs.
+			Projectile.penetrate = -1; // The yoyo projectile has infinite piercing.
+			Projectile.scale = 1f; // How large to render the projectile graphic. Some vanilla yoyos have a larger scale, such as the Kraken at 1.2f or the Eye of Cthulhu at 1.15f.
+			Projectile.DamageType = DamageClass.Melee; // This projectile deals Melee damage.
+
+			// Here we use the yoyo aiStyle(99) and copy the Cascade's unique AI code, which only covers the dust particles it generates. We will have to adapt other parts of the code that we want to use.
+			Projectile.aiStyle = ProjAIStyleID.Yoyo;
+			AIType = ProjectileID.Cascade;
+		}
+
+		// The following Method is an additional behaviour of the Cascade that is not automatically inherited through the use of Projectile.aiStyle and AIType.
+		// When we hit an NPC, there is a 1 in 3 chance of applying the On Fire debuff to them for 1 to 4 seconds.
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) {
+			if (Main.rand.NextBool(3)) {
+				target.AddBuff(BuffID.OnFire, Main.rand.Next(60, 240));
+			}
+		}
+	}
+}
